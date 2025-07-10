7db898bd00f7e275e4c3030213958abadef1d181
Giedrius R. <gr@veikia.net>
2025-07-11 00:10:20 +0300

    fix for pio, fix wdt reset

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 // #define HELTEC_NO_RADIOLIB
 #define BUTTON 21                       // Redefine button pin 
 #define ARDUINO_heltec_wifi_32_lora_V3  // Without this line, Lora Radio doesn't work with Heltec lib
+// #define RADIOLIB_DEBUG_PROTOCOL 1
 
 #include "heltec_unofficial.h"
 #include <LoRaWAN_ESP32.h>
@@ -42,12 +43,15 @@ SemaphoreHandle_t sem_LoraData;
 /************ SETUP *****************/
 
 void setup() {
+  // Serial.begin(115200);
   heltec_setup();
-  analogReadResolution( 12 );
-  analogSetPinAttenuation( VBAT_ADC, ADC_0db );
+  delay( 5000 );
+  Serial.println( "Starting..." );
+  // analogReadResolution( 12 );
+  // analogSetPinAttenuation( VBAT_ADC, ADC_0db );
   // pinMode( 46, INPUT_PULLUP );
-  pinMode( 46, OUTPUT );
-  digitalWrite( 46, HIGH );
+  // pinMode( 46, OUTPUT );
+  // digitalWrite( 46, HIGH );
   float mv = analogReadMilliVolts( 12 ); // dummy read
 
   display.landscape();
@@ -60,6 +64,18 @@ void setup() {
     display.printCenter( "Starting..." );
   }
   vTaskDelay( 3000 );
+  Serial.println( "Started." );
+
+  persist.setConsole( Serial );
+  // persist.wipe();
+  Serial.printf( "band: '%s'\n", persist.getBand() );
+  if ( persist.isProvisioned() ) {
+    Serial.println( "provisioned" );
+  }
+  else {
+    Serial.println( "NOT provisioned" );
+    persist.provision();
+  }
 
   sem_CalculatedVoltage = xSemaphoreCreateBinary();
   xSemaphoreGive( sem_CalculatedVoltage );
@@ -68,9 +84,10 @@ void setup() {
   // xTaskCreate( vTaskLed, "LED", 1024 * 2, (void*)1, tskIDLE_PRIORITY, NULL );
   xTaskCreate( vTaskEink, "EINK", 1024 * 3, (void*)1, tskIDLE_PRIORITY, NULL );
   xTaskCreate( vTaskADC, "ADC", 1024 * 3, (void*)1, tskIDLE_PRIORITY, NULL );
-  xTaskCreate( vTaskLora, "LORA", 1024 * 6, (void*)1, tskIDLE_PRIORITY, NULL );
+  xTaskCreate( vTaskLora, "LORA", 1024 * 8, (void*)1, tskIDLE_PRIORITY, NULL );
 
-  log_i( "Init." );
+  // log_i( "Init." );
+  Serial.println( "Init." );
 
   // lora();
 }
@@ -104,19 +121,19 @@ portTASK_FUNCTION( vTaskEink, pvParameters ) {
   for ( ;; ) {
     // vbat = analogReadMilliVolts( VBAT_ADC ) / 200.58;
     vbatp = heltec_battery_percent( CalculatedVoltage );
-    temp = 0; //heltec_temperature();
+    temp = heltec_temperature();
 
-    Serial.printf( "Temperature: %.1f °C\n", temp );
+    // Serial.printf( "Temperature: %.1f °C\n", temp );
 
     unsigned long t1 = millis();
     DRAW( display ) {
       display.setTextSize( 2 );
       display.setCursor( 160, 0 );
       display.printf( "%3d%% %.3fV", vbatp, CalculatedVoltage );
-      display.setCursor( 230, 20 );
-      display.printf( "%.1fC", temp );
+      display.setCursor( 220, 20 );
+      display.printf( "%.1f^C", temp );
       display.setCursor( 0, 40 );
-      display.printf( "millis: %ld", pdTICKS_TO_MS( xTaskGetTickCount() ) );
+      display.printf( "cnt: %u  millis: %ld", count, pdTICKS_TO_MS( xTaskGetTickCount() ) );
       // display.drawLine( 0, 20 - 3, 295, 20 - 3, BLACK );
       // display.drawLine( 0, 40 - 3, 295, 40 - 3, BLACK );
       display.drawLine( 0, 60 - 3, 295, 60 - 3, BLACK );
@@ -146,25 +163,29 @@ portTASK_FUNCTION( vTaskADC, pvParameters ) {
   // const float r2 = 100000.0f; // R2 in ohm, 10k potentiometer
   // float    vRefScale = (3.3f / 4096.0f) * ((r1 + r2) / r2);
   const TickType_t xFrequency = 1000 / 16; //delay for mS
+  static TickType_t xLastWakeTime = xTaskGetTickCount();
+
   float    adcValue = 0.0f;
   float    Vbatt = 0.0f;
   int      printCount = 0;
   SimpleKalmanFilter KF_ADC_b( 1.0f, 1.0f, .01f );
   uint64_t TimePastKalman = esp_timer_get_time(); // used by the Kalman filter UpdateProcessNoise, time since last kalman calculation
-  TickType_t xLastWakeTime = xTaskGetTickCount();
 
   for ( ;;) {
     // analogReadRaw( VBAT_ADC ); //read and discard
-    analogRead( 7 ); //read and discard
-    adcValue = float( analogReadMilliVolts( 7 ) ); //take a raw ADC reading
+    // analogRead( 7 ); //read and discard
+    // adcValue = float( analogReadMilliVolts( 7 ) ); //take a raw ADC reading
+    adcValue = heltec_vbat();
     KF_ADC_b.setProcessNoise( (esp_timer_get_time() - TimePastKalman) / 1000000.0f ); //get time, in microsecods, since last readings
     adcValue = KF_ADC_b.updateEstimate( adcValue ); // apply simple Kalman filter
-    Vbatt = adcValue / 204.5;//vRefScale;
+    Vbatt = adcValue;// / 204.5;//vRefScale;
     xSemaphoreTake( sem_CalculatedVoltage, portMAX_DELAY );
     CalculatedVoltage = Vbatt;
     xSemaphoreGive( sem_CalculatedVoltage );
     // log_i( "mV: %f vbat: %.4f", adcValue, Vbatt );
-    vTaskDelay(100);
+    // Serial.printf( "mV: %f vbat: %.4f\n", adcValue, Vbatt );
+    vTaskDelay( 100 );
+    // xTaskDelayUntil( &xLastWakeTime, xFrequency );
 
 #if 0
     printCount++;
@@ -186,7 +207,7 @@ portTASK_FUNCTION( vTaskADC, pvParameters ) {
 
 void loop() {
   heltec_loop();
-
+  delay( 1 ); // IMPORTANT because of wdt reset
 }
 
 
@@ -194,9 +215,9 @@ void loop() {
 
 portTASK_FUNCTION( vTaskLora, pvParameters ) {
 
-  const TickType_t xFrequency = 10000;
+  const TickType_t xFrequency = 15000;
   TickType_t xLastWakeTime = xTaskGetTickCount();
-  bool initialized = false;
+  // bool initialized = false;
   int16_t state;
 
   for ( ;;) {
@@ -207,9 +228,9 @@ portTASK_FUNCTION( vTaskLora, pvParameters ) {
     }
     else {
       log_i( "Radio initialized." );
-      initialized = true;
+      // initialized = true;
 
-      node = persist.manage( &radio );
+      node = persist.manage( &radio, true );
 
       if ( !node->isActivated() ) {
         log_e( "Could not join network. We'll try again later." );
@@ -220,13 +241,13 @@ portTASK_FUNCTION( vTaskLora, pvParameters ) {
         // If we're still here, it means we joined, and we can send something
 
           // Manages uplink intervals to the TTN Fair Use Policy
-        node->setDutyCycle( true, 1250 );
+        node->setDutyCycle( true, 250 );
 
         uint8_t uplinkData[2];
         LoRaWANEvent_t evUp, evDown;
         uplinkData[0] = count++;
         float temp = heltec_temperature();
-        uplinkData[1] = temp + 100;
+        uplinkData[1] = temp;
 
         uint8_t downlinkData[256];
         size_t lenDown = sizeof( downlinkData );
@@ -276,9 +297,8 @@ portTASK_FUNCTION( vTaskLora, pvParameters ) {
       }
     }
     persist.saveSession( node );
-    log_i( "stack: %d", uxTaskGetStackHighWaterMark( NULL ) );
-    xLastWakeTime = xTaskGetTickCount();
-    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+    log_w( "stack: %d", uxTaskGetStackHighWaterMark( NULL ) );
+    xTaskDelayUntil( &xLastWakeTime, xFrequency );
     // goToSleep();  // Does not return, program starts over next round
   }
   vTaskDelete( NULL );
